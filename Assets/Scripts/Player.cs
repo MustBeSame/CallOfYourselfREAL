7be1a41912dda210142dd3c362337d00b08446ba
GabrielAdamski <gabriel.adamski.oliveira@gmail.com>
2018-06-20 18:30:57 -0300

    servidor renovado

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,12 +7,29 @@ public class Player : MonoBehaviour
 {
     //Objecto classe player
     public int idPlayer;
-    public int servants, influence, level;
+    public int idTable;
+    public int servants, influence, level, handCount;
+    public string ip;
+    public char roomCreator;
 
     public List<Card> hand = new List<Card>();
     public Stack<Card> playStack = new Stack<Card>();
 
     public Text serv, inf;
+
+    public Player(int idPlayer, int servants, int influence, int level, int handCount, string ip)
+    {
+        this.idPlayer = idPlayer;
+        this.servants = servants;
+        this.influence = influence;
+        this.level = level;
+        this.handCount = handCount;
+        this.ip = ip;
+    }
+
+    public Player() {
+        
+    }
 
     public void setServants(int s) { this.servants = s; serv.text = servants.ToString(); }
     public void setInfluence(int i) { this.influence = i; inf.text = influence.ToString(); }
@@ -21,6 +38,8 @@ public class Player : MonoBehaviour
     public int getInfluence() { return this.influence; }
 
     public int getLevel() { return this.level; }
+
+
 
     private void Start()
     {
